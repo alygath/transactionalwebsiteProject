a278e70f7d241e6e4e4b6f8af619c7f208ea543d
Aled <aevans1@cavc.ac.uk>
2025-07-28 14:58:58 +0100

    Add initial website files and structure

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
      style="background-image: url('./images/hero.jpg'); height: 80vh; background-size: cover; background-position: left 20%;">
   <div class="bg-dark bg-opacity-50 w-100 h-100 d-flex flex-column justify-content-center align-items-center">
     <h1 class="display-4">Explore the outdoors in style</h1>
-    <p class="lead">Discover our latest collection of outdoor apparel and accessories, designed for the modern explorer..</p>
+    <p class="lead">Discover our latest collection of lovely outdoor apparel and accessories, designed for the modern explorer..</p>
     <a href="#" class="btn btn-primary mt-3">Shop now</a>
   </div>
 </div>
